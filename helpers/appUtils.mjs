5ef3a594268a4d0c55d502c6263c992ff9af9313
Charles Shelton <vbsmbb@gmail.com>
2023-04-21 14:08:01 -0400

    Updated the app utilities. Added error checker for parameters and exited if they were missing.

--- helpers/appUtils.mjs
+++ helpers/appUtils.mjs
@@ -3,11 +3,25 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 
 function getJSONData(path, jsonFile) {
+  if ((path = undefined) || jsonFile === undefined) {
+    console.error(
+      "You must pass an application path and a file name to this function (getJSONData)!"
+    );
+    process.exit(-1);
+  }
+
   const require = createRequire(path);
   return require(jsonFile);
 }
 
 function getAppPath(path) {
+  if (path === undefined) {
+    console.error(
+      "You must pass a path for the application to this function (getAPpPath)!"
+    );
+    process.exit(-1);
+  }
+
   return dirname(fileURLToPath(path));
 }
 
