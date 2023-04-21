7437488c1d26f75970659eeea1cd4d1608a0969a
Charles Shelton <vbsmbb@gmail.com>
2023-04-21 16:56:12 -0400

    Updated the getJSONData function to work from any path.

--- helpers/appUtils.mjs
+++ helpers/appUtils.mjs
@@ -1,17 +1,21 @@
 import { createRequire } from "module";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import { join } from "path";
 
 function getJSONData(path, jsonFile) {
-  if ((path = undefined) || jsonFile === undefined) {
+  if (path === undefined || jsonFile === undefined) {
     console.error(
       "You must pass an application path and a file name to this function (getJSONData)!"
     );
     process.exit(-1);
   }
 
-  const require = createRequire(path);
-  return require(jsonFile);
+  const appPath = getAppPath(path);
+  const fileName = join(appPath, jsonFile);
+  const require = createRequire(fileName);
+
+  return require(fileName);
 }
 
 function getAppPath(path) {
